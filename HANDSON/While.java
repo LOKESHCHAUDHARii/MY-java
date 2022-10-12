bd5fb4c16c9d8526f5a96de8fa8e7bdb1cbec89f
LOKESHCHAUDHARii <113008063+LOKESHCHAUDHARii@users.noreply.github.com>
2022-10-12 12:35:09 +0530

    Add files via upload

---
+++ HANDSON/While.java
@@ -0,0 +1,30 @@
+package lokesh;
+
+import java.util.Scanner;
+
+public class While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the positive  Number:");
+		 int num = sc.nextInt();
+		  sc.close();
+		  
+	while(!(num<= 0))
+		  
+{ 
+		  
+	System.out.println("welcome to the java world");
+	num++;
+	break;
+	
+}
+		
+		
+		
+		
+	}
+
+}
